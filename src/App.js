ad9f7672d206d68147803c7114a7cb477a687da3
imam <imammahadi651@gmail.com>
2020-05-16 01:46:46 +0600

    [ommit][console.log]

--- src/App.js
+++ src/App.js
@@ -20,19 +20,16 @@ const App = () => {
       const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
       const data = await response.json();
       setRecipes(data.hits);
-      console.log(data.hits)
   };
 
   const updateSearch = e => {
       setSearch(e.target.value);
-      console.log("updatesearch",search)
   }
 
   const getSearch = e => {
       e.preventDefault();
       setQuery(search);
       setSearch("");
-      console.log("getsearch",search)
   }
   return(
       <div className="App">
